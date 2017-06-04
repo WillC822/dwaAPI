3fe86e92ac429fe5c1900ada8454a25e15130af8
Will.C <WDCarey@fullsail.edu>
2017-06-03 22:09:40 -0400

    added models for db and urlShortener to access information from db

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
-var express = require('express');        //load express module set's as a var
-var bodyparser = require('body-parser'); //load bodyparser module set's as a var
-var app = express();                     //instantiate express as var
+const express = require('express');        //load express module set's as a var
+const bodyparser = require('body-parser'); //load bodyparser module set's as a var
+const app = express();                     //instantiate express as var
 
+const port = process.env.PORT || 3000; //load port 3000 as var
 
 app.use('/api', require('../routes/urlshortener.js')(express)); //sets the base of the url
 
-var port = 3000; //load port 3000 as var
-
-var server = app.listen(port, function(){              //listens to server and prints to terminal
+exports.server = app.listen(port, () => {              //listens to server and prints to terminal
   console.log('Our server is running on Port', port);
 });
 
+
+module.exports = app;
 module.exports = server; //exports the server
